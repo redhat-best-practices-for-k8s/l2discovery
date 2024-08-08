361da7da3410a7035429b245bbe33cc365d66a8f
David Elie-Dit-Cosaque <86730676+edcdavid@users.noreply.github.com>
2024-08-08 16:34:19 -0500

    Update quay organization (#56)

--- scripts/image.sh
+++ scripts/image.sh
@@ -2,7 +2,7 @@
 set -x
 VERSION=latest
 IMAGE_TAG=l2discovery
-REPO=quay.io/testnetworkfunction
+REPO=quay.io/redhat-best-practices-for-k8s
 go build l2discovery.go
 podman build -t ${IMAGE_TAG} --rm -f Dockerfile .
 
