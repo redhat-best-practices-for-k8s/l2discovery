56234c65e8a5dad0070216ac621995b1e8d7f89d
Nishant Parekh <nparekh@redhat.com>
2024-08-13 17:29:43 -0400

    added logic to extract subsystem information from lspci command  * address review comments and include the lib-exports changes Signed-off-by: Nishant Parekh <nparekh@redhat.com>

--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.21.4
 require github.com/sirupsen/logrus v1.9.3
 
 require (
-	github.com/redhat-best-practices-for-k8s/l2discovery-exports v0.0.4
+	github.com/redhat-best-practices-for-k8s/l2discovery-exports v0.0.5
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
---
+++ l2discovery_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func Test_parseLspci(t *testing.T) {
+	type args struct {
+		output string
+	}
+	tests := []struct {
+		name string
+		args args
+		exDesc string
+		exSubsystem string
+		parseErr bool
+	}{
+		{
+			name: "ok",
+			args: args{
+				output: "ca:00.0 Ethernet controller: Intel Corporation Ethernet Controller E810-C for SFP (rev 02)\nSubsystem: Intel Corporation Ethernet Network Adapter E810-XXV-4T\nControl: I/O- Mem+ BusMaster+ SpecCycle- MemWINV- VGASnoop- ParErr- Stepping- SERR- FastB2B- DisINTx+\n",
+			},
+			exDesc: "Ethernet controller: Intel Corporation Ethernet Controller E810-C for SFP (rev 02)",
+			exSubsystem: "Intel Corporation Ethernet Network Adapter E810-XXV-4T",
+			parseErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			desc, subsystem, err := parseLspci(tt.args.output)
+			if (err != nil) != tt.parseErr {
+				t.Errorf("parseLspci() error = %v, wantErr %v", err, tt.parseErr)
+				return
+			}
+			if desc != tt.exDesc {
+				t.Errorf("parseLspci() description does not match got = %v, want %v", desc, tt.exDesc)
+			}
+			if subsystem != tt.exSubsystem {
+				t.Errorf("parseLspci() subsystem does not match got = %v, want %v", subsystem, tt.exSubsystem)
+			}
+		})
+	}
+}
